9e3603f66268a88111c9baddd2e7c687edfbce97
Bokka Rakesh <borakesh@deloitte.com>
2022-07-27 20:40:38 +0530

    learned javascript basics, routing , npm packges and debugging

--- first-app.js
+++
@@ -1,4 +0,0 @@
-console.log("test for nodejs");
-const fs = require('fs');
-
-fs.writeFileSync('hello-text.js', `console.log('created hello text file')`);
--- hello-text.js
+++
@@ -1,1 +0,0 @@
-console.log('created hello text file')

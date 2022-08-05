21df257586ab301e25d22ec33b3f78ed10e2438d
Bokka Rakesh <borakesh@deloitte.com>
2022-08-05 20:58:22 +0530

    pug, ejs, handlebars, html

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.set('views', 'views')
 const bodyParser = require('body-parser');
 
 const adminData = require('./routes/admin');
